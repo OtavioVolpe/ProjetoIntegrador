8f58cb5b8383adcbfcea57c162d9753d24312672
Hernandes de Castro Guereta <132833897+Nandesfront@users.noreply.github.com>
2025-05-28 00:32:13 -0300

    Add files via upload

---
+++ Curso.java
@@ -0,0 +1,38 @@
+public class Curso {
+    private String nome;
+    private String descricao;
+    private int codigo;
+    private int cargaHoraria;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+}
